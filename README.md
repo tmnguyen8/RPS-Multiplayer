de9292d89e717a8148bca47728fedbea8c835c58
Trai Nguyen <tnguyen@at-pac.com>
2019-09-30 09:37:28 -0400

    initialize commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # RPS-Multiplayer
+Rock Paper Scissors Multiplayer
+
+Only two users can play at the same time.
+
+Both players pick either rock, paper or scissors. After the players make their selection, the game will tell them whether a tie occurred or if one player defeated the other.
+
+The game will track each player's wins and losses.
---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// player enters their name
+// players see available players
+// click play to initiate the game
+// player 1 select rock, paper or scissors
+// notify player 1 has selected
+// player 2 select rock, paper or scissors
+// notify player 2 has selected
+// display result of who wins
+// keep track of how many each player wins (winCount)
+// players see available players
