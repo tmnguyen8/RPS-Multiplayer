5bc49bfcd2509d8bbdc09e2514c12cdb2ea94d39
Trai Nguyen <tnguyen@at-pac.com>
2019-09-30 16:30:48 -0400

    added js

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,49 @@
 // display result of who wins
 // keep track of how many each player wins (winCount)
 // players see available players
+
+// GLOBAL VARIABLES
+// ***************************************************
+// initialize firebase
+var config = {
+    apiKey: "AIzaSyBh8P6xa6Ln7RQnjfJBFH8caczJg2IIQmc",
+    authDomain: "rpg-multiple-player.firebaseapp.com",
+    databaseURL: "https://rpg-multiple-player.firebaseio.com",
+    projectId: "rpg-multiple-player",
+    storageBucket: ""
+};
+
+firebase.initializeApp(config);
+// Create a variable to reference the database
+var database = firebase.database();
+
+// global object of player template
+class player {
+    constructor(name, status) {
+        this.name = name;
+        this.status = status;
+    }
+    doSomething () {
+        console.log(`${this.name} is working`)
+    }
+};
+
+// currentGame 
+
+// FUNCTIONS
+// ***************************************************
+function displayPlayer(player) {
+    var newPlayer = $(`
+        <div class="player" data-name"${player.name}">${player.name}</div>
+        <button class="play-game btn btn-secondary">Play</button>
+    `);
+    $(".available-player").append(newPlayer);
+}
+
+// EVENTS / EXECUTION
+// ***************************************************
+$(document).on("click", "#submit-btn", function(event){
+    event.preventDefault();
+    var newPlayer = new player($("#player-name").val().trim(), "available");
+    displayPlayer(newPlayer)
+});
